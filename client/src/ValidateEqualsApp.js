95e02b3514e6ce58fcf26ce634200b7e2ea28dc5
Barak Bar Orion <barak.bar@gmail.com>
2013-09-28 12:59:37 +0300

    Add error message to password validation

--- client/src/ValidateEqualsApp.js
+++ client/src/ValidateEqualsApp.js
@@ -4,10 +4,25 @@
         return {
             require: 'ngModel',
             link: function (scope, elm, attrs, ngModelController) {
+                var errorMsg = scope.$eval(attrs.errorMsg);
+                if(errorMsg){
+                    var helpEl = angular.element("<span>").addClass("help-inline ng-hide hide").text(errorMsg);
+                    elm.after(helpEl);
+                }
                 function validateEquals(myValue) {
                     var valid = (myValue === scope.$eval(attrs.validateEquals));
                     ngModelController.$setValidity('equals', valid);
-                    return valid ? myValue : undefined;
+                    if(valid){
+                        if(helpEl){
+                            helpEl.addClass("ng-hide hide");
+                        }
+                        return myValue;
+                    }else{
+                        if(helpEl){
+                            helpEl.removeClass("ng-hide hide");
+                        }
+                        return undefined;
+                    }
                 }
 
                 ngModelController.$parsers.push(validateEquals);
